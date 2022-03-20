1171a5ee395bb52c5ec94829ab61f3ac98e7b95a
Kiddo amdu <62068502+kidistamdu@users.noreply.github.com>
2022-03-20 15:23:20 +0800

    imported firebase script

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCG9T83u32uxFZlO-C6ojg-1nAz82OAcfs",
+    authDomain: "to-do-list-21803.firebaseapp.com",
+    projectId: "to-do-list-21803",
+    storageBucket: "to-do-list-21803.appspot.com",
+    messagingSenderId: "921664842227",
+    appId: "1:921664842227:web:123603b809d63d6b3b0a86",
+    measurementId: "G-SRLLX9Z8BN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
