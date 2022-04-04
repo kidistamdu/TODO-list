c1a1ce96ab34ec99000b68faa2dc145d3bc149f5
Kiddo amdu <62068502+kidistamdu@users.noreply.github.com>
2022-04-04 12:02:08 +0800

    added  generateItems

--- script.js
+++ script.js
@@ -11,6 +11,40 @@ function addItem(event){
 
 function getItems(){
     db.collection("todo-items").onSnapshot((snapshot) => {
-        
+        console.log(snapshot);
+        let items = [];
+        snapshot.docs.forEach((doc)=>{
+            items.push({
+                id: doc.id,
+                ...doc.data()
+            })
+        } )
+        generateItems(items)
     }) 
 }
+    function generateItems(items){
+
+        let itemsHTML = "";
+        items.forEach((items)=>{
+            console.log(items);
+
+            itemsHTML += `
+            <div class="todo-item">
+            <div class="check">
+                <div class="check-mark">
+                    <img src="/images/icon-check.svg">
+                </div>
+            </div>
+            <div class="todo-text">
+                ${item.text}
+            </div>
+        </div>
+               
+            `
+        })
+        document.querySelector(".todo-items").innerHTML = itemsHTML;
+
+    }
+
+
+getItems();
